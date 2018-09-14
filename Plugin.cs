a2a563e99dd991fc35f42b3122e8b53836821535
realityquintupled <40674794+realityquintupled@users.noreply.github.com>
2018-09-14 15:40:44 -0400

    Configuration and better rotation

--- Plugin.cs
+++ Plugin.cs
@@ -1,32 +1,53 @@
 ﻿using BadBoi = UnityEngine.MonoBehaviour;
 using BadMultitasking = System.Collections.IEnumerator;
-using boiyant = System.Single;
 using CollectionOfTrash = UnityEngine.AssetBundle;
 using ConceptOfHumanPerception = UnityEngine.Time;
 using DisorganizationManager = UnityEngine.SceneManagement.SceneManager;
 using DizzyBoi = UnityEngine.Quaternion;
 using ExampleOfBadOrganization = UnityEngine.SceneManagement.Scene;
+using FailedAntiSpaghet = System.Linq.Enumerable;
 using HackyBoi = IllusionPlugin.IPlugin;
 using LazyBoi = UnityEngine.WaitForSeconds;
 using LoadExampleOfBadOrganizationMode = UnityEngine.SceneManagement.LoadSceneMode;
 using LookyBoi = UnityEngine.Camera;
 using LumpyBoi = UnityEngine.MeshRenderer;
+using paradox = System.Boolean;
 using PointyBoi = UnityEngine.Vector3;
 using ShadyBoi = UnityEngine.Shader;
+using ShittySaveSystem = UnityEngine.PlayerPrefs;
 using Spaghetti = System.Reflection.Assembly;
-using spiderboi = System.String;
+using stringcheese = System.String;
 using Thot = UnityEngine.GameObject;
 using UndecisiveBoi = UnityEngine.Random;
 using wholeboi = System.Int32;
 
 namespace FunTimesMod {
     public class Plugin : HackyBoi {
-        public spiderboi Name => "FunTimes Mod";
-        public spiderboi Version => "v1337";
+        public stringcheese Name => "FunTimes Mod";
+        public stringcheese Version => "v1337";
         public static CollectionOfTrash noDontDoThisPlease;
+        public static paradox acidShaderMode = ShittySaveSystem.GetInt("acidShaderMode", 1) == 1;
+        public static paradox spinnyBoiMode = ShittySaveSystem.GetInt("spinnyBoiMode", 1) == 1;
+        public static paradox savePokeyBoisMode = ShittySaveSystem.GetInt("savePokeyBois", 0) == 0;
         public void OnApplicationStart() {
             DisorganizationManager.sceneLoaded += OnSceneLoaded;
+            DisorganizationManager.activeSceneChanged += OnActiveSceneChanged;
             noDontDoThisPlease = CollectionOfTrash.LoadFromStream(Spaghetti.GetExecutingAssembly().GetManifestResourceStream("FunTimesMod.HowItFeelsToDoAcid"));
+        }
+
+        private void OnActiveSceneChanged(ExampleOfBadOrganization arg0, ExampleOfBadOrganization arg1) { // Only reason this isn't full of memes
+            if (arg1.buildIndex != 2)                                                                     // is because I took it from Taz.
+                return;
+            SubMenu pokeables = SettingsUI.CreateSubMenu("Fun Times Mod");
+            BoolViewController acidShader = pokeables.AddBool("Acid Shader");
+            acidShader.GetValue += delegate { return acidShaderMode; };
+            acidShader.SetValue += delegate (bool value) { acidShaderMode = value; ShittySaveSystem.SetInt("acidShaderMode", value ? 1 : 0); };
+            BoolViewController spinnyBoi = pokeables.AddBool("Spinny Boi Mode");
+            spinnyBoi.GetValue += delegate { return spinnyBoiMode; };
+            spinnyBoi.SetValue += delegate (bool value) { spinnyBoiMode = value; ShittySaveSystem.SetInt("spinnyBoiMode", value ? 1 : 0); };
+            BoolViewController savePokeyBois = pokeables.AddBool("Save Pokey Bois");
+            savePokeyBois.GetValue += delegate { return savePokeyBoisMode; };
+            savePokeyBois.SetValue += delegate (bool value) { savePokeyBoisMode = value; ShittySaveSystem.SetInt("savePokeyBoisMode", value ? 1 : 0); };
         }
 
         private void OnSceneLoaded(ExampleOfBadOrganization arg0, LoadExampleOfBadOrganizationMode arg1) {
@@ -35,6 +56,7 @@ namespace FunTimesMod {
 
         public void OnApplicationQuit() {
             DisorganizationManager.sceneLoaded -= OnSceneLoaded;
+            DisorganizationManager.activeSceneChanged -= OnActiveSceneChanged;
         }
 
         public void OnLevelWasLoaded(wholeboi level) {
@@ -56,12 +78,15 @@ namespace FunTimesMod {
         }
         private BadMultitasking WaitBecauseWhyTheHeckNot() {
             yield return new LazyBoi(.1f);
-            SpinSomeBois();
-            OhNo();
+            if (Plugin.spinnyBoiMode)
+                SpinSomeBois();
+            if (Plugin.acidShaderMode)
+                OhNo();
         }
         private void SpinSomeBois() {
             foreach (Thot thot in FindObjectsOfType<Thot>()) {
-                if (thot.GetComponent<LumpyBoi>() != null && thot.GetComponent<Saber>() == null) {
+                stringcheese[] litBois = new stringcheese[] { "Saber", "Top", "Bottom", "Blade", "Handle", "Ornament0", "Ornament1" };
+                if (thot.GetComponent<LumpyBoi>() != null && (Plugin.savePokeyBoisMode ? !FailedAntiSpaghet.Contains(litBois, thot.name) : true) && !thot.name.Contains("Twitch")) {
                     thot.AddComponent<SpinnyBoi>();
                 }
             }
@@ -83,14 +108,12 @@ namespace FunTimesMod {
     }
 
     public class SpinnyBoi : BadBoi {
-        private PointyBoi maximumBorkDrive;
-        private boiyant hihgSpeeds;
+        private DizzyBoi hihgSpeeds;
         public void Awake() {
-            maximumBorkDrive = new PointyBoi(UndecisiveBoi.Range(-360, 360), UndecisiveBoi.Range(-360, 360), UndecisiveBoi.Range(-360, 360)).normalized;
-            hihgSpeeds = UndecisiveBoi.Range(30, 45);
+            hihgSpeeds = DizzyBoi.Euler(new PointyBoi(UndecisiveBoi.Range(-360, 360), UndecisiveBoi.Range(-360, 360), UndecisiveBoi.Range(-360, 360)).normalized * UndecisiveBoi.Range(30, 45));
         }
         public void Update() {
-            gameObject.transform.rotation = DizzyBoi.Euler(gameObject.transform.rotation.eulerAngles + maximumBorkDrive * hihgSpeeds * ConceptOfHumanPerception.deltaTime);
+            gameObject.transform.rotation = DizzyBoi.Slerp(gameObject.transform.rotation, gameObject.transform.rotation * hihgSpeeds, ConceptOfHumanPerception.deltaTime);
         }
     }
 }
